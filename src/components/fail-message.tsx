34beb476fd5d169706532ba9dfe46fa3990bc493
Andrey Kudinov <2499931@gmail.com>
2022-02-04 23:51:43 +0400

    add FailMessage and SuccessMessage components

---
+++ src/components/fail-message.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const FailMessage = () => (
+  <div className='reviews__message'>
+    <span className='icon-important-info reviews__icon' />
+    <h4>Sorry, something went wrong!</h4>
+    <p className='reviews__text'>Try again later.</p>
+  </div>
+)
---
+++ src/components/success-message.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SuccessMessage = () => (
+  <div className='reviews__message'>
+    <span className='icon-check-circle-outline reviews__icon' />
+    <h4>Thank you for you review!</h4>
+    <p className='reviews__text'>Your review has been submitted.</p>
+  </div>
+)
