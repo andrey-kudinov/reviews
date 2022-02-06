388d12ff1baddaa626fa33309c555356745fd2c1
Andrey Kudinov <2499931@gmail.com>
2022-02-06 23:40:32 +0400

    add props to main

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { cardsData, productData } from './data/data'
+import { randomDate, randomRating } from './helpers/helpers'
 import Reviews from './Reviews'
+
+cardsData.forEach((card) => {
+  card.date = String(randomDate(new Date(2012, 0, 1), new Date()))
+  card.rating = randomRating(1, 5)
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <Reviews />
+    <Reviews cardsData={cardsData} productData={productData} />
   </React.StrictMode>,
   document.getElementById('root')
 )
