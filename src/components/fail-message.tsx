80c333d287cf08b4d30ba0bc036843baf99b2292
Andrey Kudinov <2499931@gmail.com>
2022-02-10 22:16:05 +0400

    add message

--- src/components/fail-message.tsx
+++ src/components/fail-message.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const FailMessage = () => (
   <div className='reviews__message'>
-    <span className='icon-important-info reviews__icon' />
     <h4>Sorry, something went wrong!</h4>
-    <p className='reviews__text'>Try again later.</p>
+    <p className='reviews__text'>
+      Try again later. Check out the DevTools Console anyway!
+    </p>
   </div>
 )
