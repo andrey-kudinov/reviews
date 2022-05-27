0669058eb686a91e6320f25c29be16e84e2dc625
Andrey Kudinov <2499931@gmail.com>
2022-05-27 23:44:04 +0400

    Clean up a bit

--- .prettierrc.js
+++ .prettierrc.js
@@ -9,23 +9,3 @@ module.exports = {
   semi: false,
   jsxSingleQuote: true
 }
-
-// {
-//   "arrowParens": "always",
-//   "bracketSpacing": true,
-//   "embeddedLanguageFormatting": "auto",
-//   "htmlWhitespaceSensitivity": "css",
-//   "insertPragma": false,
-//   "jsxBracketSameLine": false,
-//   "jsxSingleQuote": false,
-//   "proseWrap": "preserve",
-//   "quoteProps": "as-needed",
-//   "requirePragma": false,
-//   "semi": true,
-//   "singleQuote": false,
-//   "tabWidth": 2,
-//   "trailingComma": "es5",
-//   "useTabs": false,
-//   "vueIndentScriptAndStyle": false,
-//   "printWidth": 100
-// }
